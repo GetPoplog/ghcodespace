a1478697029f90d5a2da84d264af9ca69c229c3b
Stephen Leach <sfkleach@gmail.com>
2024-08-05 11:13:21 +0100

    Update init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,4 @@
 echo 'deb http://download.opensuse.org/repositories/home:/getpoplog/xUbuntu_20.04/ /' | tee /etc/apt/sources.list.d/home:getpoplog.list
 curl -fsSL https://download.opensuse.org/repositories/home:getpoplog/xUbuntu_20.04/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/home_getpoplog.gpg > /dev/null
 apt update
-apt install poplog
+apt install poplog -y
